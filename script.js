0155dd0f62ddec217334e44729e0da01b982ec11
Rayhan Muhammad <rayhanmm1234@gmail.com>
2025-12-18 21:20:34 +0700

    menulis js, popup/dialog box

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 alert("Hello from script.js");
+var nama = prompt("Please enter your name:");
+alert ("Welcome, " + nama + "!");
+
+var conf = confirm("Are you ready to learn JavaScript, " + nama + "?");
+if (conf == true){
+    alert("Great! Let's get started, " + nama + "!");
+}else {
+    alert("No worries, take your time " + nama + "!");
+}
--- script_ext.js
+++ script_ext.js
@@ -1,3 +1,5 @@
 var nama = "Budi";
         console.log(nama);
         console.log("Selamat datang " + nama);
+
+alert("Halo " + nama + ", selamat belajar JavaScript!");

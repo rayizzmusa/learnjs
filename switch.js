503f2d625cdbaaf8dbd46c0db3b6d021a237c3a1
Rayhan Muhammad <rayhanmm1234@gmail.com>
2025-12-19 21:25:56 +0700

    witch

---
+++ switch.js
@@ -0,0 +1,16 @@
+var menu = prompt("Pilih menu: \n1. Nasi Goreng\n2. Mie Ayam\n3. Soto Ayam");
+
+switch(menu){
+    case "1":
+        alert("Anda memilih Nasi Goreng.");
+        break;
+    case "2":
+        alert("Anda memilih Mie Ayam.");
+        break;
+    case "3":
+        alert("Anda memilih Soto Ayam.");
+        break;
+    default:
+        alert("Menu tidak tersedia.");
+        
+}

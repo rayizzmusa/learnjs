1918011616e059104f24980711c3f27993caf1b6
Rayhan Muhammad <rayhanmm1234@gmail.com>
2025-12-21 14:42:50 +0700

    parameter dan argument function js

--- paramargumen.js
+++ paramargumen.js
@@ -12,3 +12,14 @@ let hasil;
 
 hasil = tambah(kali(bil1), kali(bil2)); //kali1, kali2 disini argumen
 console.log(hasil);
+
+
+function tambah2(){ // jika tidak ada parameternya
+    let hasil = 0;
+    for (i = 0; i<arguments.length;i++){
+        hasil += arguments[i];
+    }
+    return hasil;
+}
+
+console.log(tambah2(2,2,2));

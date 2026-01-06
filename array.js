1feb2a605f3cfa93a050518527269d7159a9980d
Rayhan Muhammad <rayhanmm1234@gmail.com>
2026-01-06 21:22:25 +0700

    array

---
+++ array.js
@@ -0,0 +1,6 @@
+//jika variable biasa
+var hari1 = "senin";
+var hari2 = "selasa"; // ini akan susah dimanage jika banyak hari
+
+//jika menggunakan array
+var hari = ["senin", "selasa", "rabu", "kamis"];

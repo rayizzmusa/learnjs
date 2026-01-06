0ddbec3dc5156818062d63fc50133f4112e1b024
Rayhan Muhammad <rayhanmm1234@gmail.com>
2026-01-06 20:49:48 +0700

    rekursif, rekursif faktorial

--- rekursif.js
+++ rekursif.js
@@ -20,3 +20,11 @@ function tampilAngka(n){
 }
 
 tampilAngka(10);
+
+//contoh lain rekursif adalah faktorial
+function faktorial (n){
+    if (n === 0) return 1;
+    return n * faktorial(n - 1);
+}
+
+console.log(faktorial(5)); // 5 x 4 x 3 x 2 x 1 = 120

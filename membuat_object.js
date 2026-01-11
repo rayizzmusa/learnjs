2e2c66cb7bed3f3d062f8406c4ddb41543587b63
Rayhan Muhammad <rayhanmm1234@gmail.com>
2026-01-11 14:14:12 +0700

    membuat object

---
+++ membuat_object.js
@@ -0,0 +1,38 @@
+//object literal
+var mhs1 = {
+    nama : "Rayhan",
+    nim : "202110370311028",
+    email : "rayhan@exc.com",
+    jurusan : "IT"
+    //dll
+}// mhs2 tinggal ganti isi propertinya
+
+//function declaration
+function buatObjectMhs(nama, nim, email, jurusan){
+    var mhs ={};
+    mhs.nama = nama;
+    mhs.nim = nim;
+    mhs.email = email;
+    mhs.jurusan = jurusan;
+    return mhs;
+}
+
+var mhs2 = buatObjectMhs("syifa", "202108", "syifa@exc.com", "B arab");
+
+//constructor
+function Mahasiswa(nama, nim, email, jurusan){
+    //yg membedakan dengan function declaration adalah penggunaan this yang sudah otomatis
+    //var this = {};
+    this.nama = nama;
+    this.nim = nim;
+    this.email = email;
+    this.jurusan = jurusan;
+    //return this;
+}
+
+
+var mhs3 = new Mahasiswa("erik", "12129", "erik@sd.com", "IT");
+
+
+
+

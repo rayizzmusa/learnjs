3d4ca4660375dd7a854008fb44e82c322bcf3dbd
Rayhan Muhammad <rayhanmm1234@gmail.com>
2026-02-03 06:13:14 +0700

    dom manipulation

---
+++ dom/dommanipulation.js
@@ -0,0 +1,38 @@
+//element.innerHTML
+// const judul=document.getElementById('judul');
+// judul.innerHTML="<em>Rayhan</em>";
+
+// const sectionA=document.querySelector('section#a');
+// sectionA.innerHTML="Hello World";
+
+//style.<property css>
+// const judul2=document.querySelector('#judul');
+// judul2.style.color='blue';
+// judul2.style.backgroundColor='lightblue';
+
+//element.setAttribute()
+// const judul3 = document.getElementsByTagName("h1")[0];
+// judul3.setAttribute('name', 'sandika');
+// const a = document.querySelector('section#a a');
+// a.setAttribute('id', 'link');
+// a.getAttribute('href'); //mendapatkan atribut href
+// a.removeAttribute('href'); //menghapus atribut href
+
+//element.classList
+// const p2 = document.querySelector('.p2');
+// p2.classList.add('label');
+// p2.classList.remove('label');
+// p2.classList.toggle('label'); //jika ada dihapus, jika tidak ada ditambah
+// // document.body.style.backgroundColor = 'lightblue';
+// document.body.classList.toggle('biru-muda');
+// document.body.classList.toggle('biru-muda');
+// p2.classList.add('label', 'tebal'); //menambah lebih dari 1 class sekaligus
+// p2.classList.item(2); //mengambil class pada index ke 2(tebal)
+// p2.classList.contains('tebal'); //mengembalikan nilai true/false apakah ada class tebal atau tidak
+// p2.classList.replace('tebal', 'miring'); //mengganti class tebal menjadi miring
+
+//element.createElement()
+
+
+
+
